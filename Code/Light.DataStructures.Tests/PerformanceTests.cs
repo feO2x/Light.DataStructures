0e3904bf7f4057edfdca6aa628373fb7e4313f6a
Kenny Pflug <kenny.pflug@live.de>
2016-12-22 09:56:29 +0100

    added SingleThreadedAdd performance test

---
+++ Code/Light.DataStructures.Tests/PerformanceTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using TestData = System.Collections.Generic.IEnumerable<object[]>;
+
+namespace Light.DataStructures.Tests
+{
+    [Trait("Category", Traits.PerformanceTests)]
+    public sealed class PerformanceTests
+    {
+        private const int NumberOfKeys = 10000000;
+
+        private static readonly int[] Keys = Enumerable.Range(1, NumberOfKeys)
+                                                       .GroupBy(number => number % 27)
+                                                       .SelectMany(group => group)
+                                                       .ToArray();
+
+        [Theory]
+        [MemberData(nameof(SingleThreadedDictionaryInstances))]
+        public void SingleThreadedAdd(IDictionary<int, object> dictionary)
+        {
+            foreach (var key in Keys)
+            {
+                dictionary.Add(key, new object());
+            }
+        }
+
+        public static readonly TestData SingleThreadedDictionaryInstances =
+            new[]
+            {
+                new object[] { new Dictionary<int, object>() },
+                new object[] { new ConcurrentDictionary<int, object>() },
+                new object[] { new LockFreeArrayBasedDictionary<int, object>() }
+            };
+    }
+}

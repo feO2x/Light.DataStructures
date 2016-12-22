b83efe7230d25c2586dae523eb39c647835561b6
Kenny Pflug <kenny.pflug@live.de>
2016-12-22 20:01:12 +0100

    added new console project for performance tests

---
+++ Code/Light.DataStructures.PerformanceTests/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace Light.DataStructures.PerformanceTests
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var targetDictionary = args[0].InstantiateTarget();
+
+            var test = new SingleThreadedAddTest();
+            var results = test.Run(targetDictionary);
+
+            results.Print();
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static IDictionary<int, object> InstantiateTarget(this string dictionaryName)
+        {
+            if (dictionaryName == "Dictionary")
+                return new Dictionary<int, object>();
+            if (dictionaryName == "ConcurrentDictionary")
+                return new ConcurrentDictionary<int, object>();
+            if (dictionaryName == "LockFreeDictionary")
+                return new LockFreeArrayBasedDictionary<int, object>();
+
+            throw new ArgumentException($"Invalid dictionary name: {dictionaryName}", nameof(dictionaryName));
+        }
+    }
+}
---
+++ Code/Light.DataStructures.PerformanceTests/SingleThreadedAddTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Light.DataStructures.PerformanceTests
+{
+    public sealed class SingleThreadedAddTest
+    {
+        private const int NumberOfKeys = 10000000;
+
+        private static readonly int[] Keys = Enumerable.Range(1, NumberOfKeys)
+                                                       .GroupBy(number => number % 27)
+                                                       .SelectMany(group => group)
+                                                       .ToArray();
+
+        public PerformanceTestResults Run(IDictionary<int, object> dictionary)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            foreach (var key in Keys)
+            {
+                dictionary.Add(key, new object());
+            }
+            stopwatch.Stop();
+
+            return new PerformanceTestResults(nameof(SingleThreadedAddTest), dictionary.GetType().Name, stopwatch.Elapsed);
+        }
+    }
+}

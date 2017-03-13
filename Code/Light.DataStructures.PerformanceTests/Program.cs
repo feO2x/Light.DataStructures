9b218e08522531982e4f70fb9f8ca9d15f92d8e1
Kenny Pflug <kenny.pflug@live.de>
2017-03-13 19:11:30 +0100

    SingleThreadedAddTest is now using Benchmark .NET

--- Code/Light.DataStructures.PerformanceTests/Program.cs
+++ Code/Light.DataStructures.PerformanceTests/Program.cs
@@ -1,72 +1,13 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
+using BenchmarkDotNet.Running;
 
 namespace Light.DataStructures.PerformanceTests
 {
     public static class Program
     {
-        public static void Main(string[] arguments)
+        public static void Main()
         {
-            try
-            {
-                var commandArgumentsMapping = new CommandLineArgumentsParser().ParseArguments(arguments);
-
-                var dictionary = InstantiateDictionary(commandArgumentsMapping);
-                var test = InstantiateTest(commandArgumentsMapping);
-
-                var testResults = test.Run(dictionary);
-
-                testResults.Print();
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-
-        private static IDictionary<int, object> InstantiateDictionary(Dictionary<string, string> commandArgumentsMapping)
-        {
-            string dictionaryTypeName;
-            if (commandArgumentsMapping.TryGetValue("testTarget", out dictionaryTypeName) == false)
-                throw new ArgumentException("\"testTarget\" is not specified in command line arguments.");
-
-            switch (dictionaryTypeName)
-            {
-                case "Dictionary":
-                    return new Dictionary<int, object>();
-                case "ConcurrentDictionary":
-                    return new ConcurrentDictionary<int, object>();
-                case "LockFreeDictionary":
-                    return new LockFreeArrayBasedDictionary<int, object>();
-            }
-
-            throw new ArgumentException($"Invalid testTarget name: {dictionaryTypeName}");
-        }
-
-        public static IPerformanceTest InstantiateTest(Dictionary<string, string> commandArgumentsMapping)
-        {
-            string testName;
-            if (commandArgumentsMapping.TryGetValue("test", out testName) == false)
-                throw new ArgumentException("\"test\" is not specified in command line arguments");
-
-            var targetType = typeof(Program).Assembly
-                                            .ExportedTypes
-                                            .FirstOrDefault(t => t.Name == testName);
-
-            if (targetType == null)
-                throw new ArgumentException($"Test \"{testName}\" does not exist.");
-
-            var createMethod = targetType.GetMethods()
-                                         .FirstOrDefault(m => m.IsStatic &&
-                                                              m.ReturnType == typeof(IPerformanceTest) &&
-                                                              m.GetParameters().Single(p => p.ParameterType == typeof(IDictionary<string, string>)) != null);
-
-            if (createMethod == null)
-                return (IPerformanceTest) Activator.CreateInstance(targetType);
-
-            return (IPerformanceTest) createMethod.Invoke(null, new object[] { commandArgumentsMapping });
+            BenchmarkRunner.Run<SingleThreadedAddTest>();
         }
     }
 }
--- Code/Light.DataStructures.PerformanceTests/SingleThreadedAddTest.cs
+++ Code/Light.DataStructures.PerformanceTests/SingleThreadedAddTest.cs
@@ -1,37 +1,96 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using BenchmarkDotNet.Attributes;
 using Light.GuardClauses;
 
 namespace Light.DataStructures.PerformanceTests
 {
-    public sealed class SingleThreadedAddTest : IPerformanceTest
+    public class SingleThreadedAddTest
     {
-        private readonly int _numberOfKeys;
+        private static readonly List<int> Keys10000;
+        private static readonly List<int> Keys100000;
+        private static readonly List<int> Keys1000000;
+        private List<int> _keys;
 
-        public SingleThreadedAddTest(int numberOfKeys)
+        static SingleThreadedAddTest()
         {
-            numberOfKeys.MustNotBeLessThan(0);
-
-            _numberOfKeys = numberOfKeys;
+            Keys10000 = InitializeList(10_000, 42);
+            Keys100000 = InitializeList(100_000, 50400302);
+            Keys1000000 = InitializeList(1_000_000, 9812903);
         }
 
-        public PerformanceTestResults Run(IDictionary<int, object> dictionary)
+        [Params(10_000, 100_000, 1_000_000)]
+        public int NumberOfItems { get; set; }
+
+
+        private static List<int> InitializeList(int numberOfItems, int seed)
         {
-            var keys = IntKeyTestInfo.CreateKeys(_numberOfKeys);
-            var stopwatch = Stopwatch.StartNew();
-            foreach (var key in keys)
+            var random = new Random(seed);
+            var list = new List<int>(numberOfItems);
+
+            while (list.Count < numberOfItems)
+            {
+                var number = random.Next();
+                if (list.Contains(number))
+                    continue;
+
+                list.Add(number);
+            }
+
+            return list;
+        }
+
+        [Setup]
+        public void Setup()
+        {
+            NumberOfItems.MustNotBe(0);
+
+            if (NumberOfItems == 10_000)
+                _keys = Keys10000;
+            else if (NumberOfItems == 100_000)
+                _keys = Keys100000;
+            else
+                _keys = Keys1000000;
+        }
+
+        [Benchmark]
+        public Dictionary<int, object> Dictionary()
+        {
+            var dictionary = new Dictionary<int, object>();
+
+            foreach (var key in _keys)
             {
                 dictionary.Add(key, new object());
             }
-            stopwatch.Stop();
 
-            return new PerformanceTestResults(nameof(SingleThreadedAddTest), dictionary.GetType().Name, stopwatch.Elapsed);
+            return dictionary;
         }
 
-        public static IPerformanceTest Create(IDictionary<string, string> parsedCommandArguments)
+        [Benchmark]
+        public ConcurrentDictionary<int, object> ConcurrentDictionary()
         {
-            var numberOfKeys = IntKeyTestInfo.GetNumberOfKeysFromArguments(parsedCommandArguments);
-            return new SingleThreadedAddTest(numberOfKeys);
+            var concurrentDictionary = new ConcurrentDictionary<int, object>();
+
+            foreach (var key in _keys)
+            {
+                concurrentDictionary.TryAdd(key, new object());
+            }
+
+            return concurrentDictionary;
+        }
+
+        [Benchmark]
+        public LockFreeArrayBasedDictionary<int, object> LockFreeArrayBasedDictionary()
+        {
+            var lockFreeDictionary = new LockFreeArrayBasedDictionary<int, object>();
+
+            foreach (var key in _keys)
+            {
+                lockFreeDictionary.TryAdd(key, new object());
+            }
+
+            return lockFreeDictionary;
         }
     }
 }

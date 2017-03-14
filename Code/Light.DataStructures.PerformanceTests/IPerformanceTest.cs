814095e1bed008530f87ff2de4e31ae55f5f1320
Kenny Pflug <kenny.pflug@live.de>
2017-03-14 09:26:43 +0100

    removed unnecessary files

--- Code/Light.DataStructures.PerformanceTests/IPerformanceTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Light.DataStructures.PerformanceTests
-{
-    public interface IPerformanceTest
-    {
-        PerformanceTestResults Run(IDictionary<int, object> dictionary);
-    }
-}
